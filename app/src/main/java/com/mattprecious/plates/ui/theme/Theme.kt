77e2392f6e881256090c2cfc4815a4803dc49c35
Matthew Precious <mattprecious@gmail.com>
2023-01-20 00:33:44 -0500

    Initial commit

---
+++ app/src/main/java/com/mattprecious/plates/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.mattprecious.plates.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors()
+
+private val LightColorPalette = lightColors()
+
+@Composable
+fun PlatesTheme(
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable () -> Unit
+) {
+  val colors = if (darkTheme) {
+    DarkColorPalette
+  } else {
+    LightColorPalette
+  }
+
+  MaterialTheme(
+    colors = colors,
+    content = content
+  )
+}
