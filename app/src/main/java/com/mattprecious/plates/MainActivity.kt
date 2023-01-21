ca7d6106561b053ca5ab02403c5c7941ae152192
Matthew Precious <mattprecious@gmail.com>
2023-01-21 11:37:32 -0500

    Move top level UI out of the activity

--- app/src/main/java/com/mattprecious/plates/MainActivity.kt
+++ app/src/main/java/com/mattprecious/plates/MainActivity.kt
@@ -3,53 +3,14 @@ package com.mattprecious.plates
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.mattprecious.plates.ui.theme.PlatesTheme
+import com.mattprecious.plates.ui.PlatesApp
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
     WindowCompat.setDecorFitsSystemWindows(window, false)
 
-    setContent {
-      val systemUiController = rememberSystemUiController()
-      val useDarkIcons = !isSystemInDarkTheme()
-
-      LaunchedEffect(systemUiController, useDarkIcons) {
-        systemUiController.setSystemBarsColor(
-          color = Color.Transparent,
-          darkIcons = useDarkIcons,
-        )
-      }
-
-      PlatesTheme {
-      }
-    }
+    setContent { PlatesApp() }
   }
 }
---
+++ app/src/main/java/com/mattprecious/plates/ui/app.kt
@@ -0,0 +1,20 @@
+package com.mattprecious.plates.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@Composable
+fun PlatesApp() {
+  val systemUiController = rememberSystemUiController()
+  val useDarkIcons = !isSystemInDarkTheme()
+
+  LaunchedEffect(systemUiController, useDarkIcons) {
+    systemUiController.setSystemBarsColor(
+      color = Color.Transparent,
+      darkIcons = useDarkIcons,
+    )
+  }
+}
